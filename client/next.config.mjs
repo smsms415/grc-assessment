c822c2798eb427c7937aee56ec444c535e22d24d
Sebastian M <160692133+smsms415@users.noreply.github.com>
2024-02-21 10:27:34 -0500

    redirect off /

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "standalone" };
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/concept/list",
+        permanent: false,
+      },
+    ];
+  },
+  output: "standalone",
+};
 
 export default nextConfig;
